c810d97d0036ae63bfc59ec51b29598a4505ba7f
Alexander Ivanov <76620843+ark-leaf@users.noreply.github.com>
2022-03-22 16:23:06 +0200

    refactor: rename log functions

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,16 @@ export const testCase = (
 }
 
 export const precondition = (msg: string, precondition: () => void) => {
-  cy.task('log_precondition', `  Precondition ${msg}`, { log: false })
+  cy.task('cypress_witch_log_precondition', `  Precondition ${msg}`, { log: false })
   precondition()
 }
 
 export const step = (msg: string, step: () => void) => {
-  cy.task('log_step', `  Step ${msg}`, { log: false })
+  cy.task('cypress_witch_log_step', `  Step ${msg}`, { log: false })
   step()
 }
 
 export const expectedResult = (msg: string, expectedResult: () => void) => {
-  cy.task('log_expected_result', `  Expected Result: ${msg}`, { log: false })
+  cy.task('cypress_witch_log_expected_result', `  Expected Result: ${msg}`, { log: false })
   expectedResult()
 }
--- src/plugins.ts
+++ src/plugins.ts
@@ -1,14 +1,14 @@
 module.exports = {
   plugins: {
-    log_precondition(message: string) {
+    cypress_witch_log_precondition(message: string) {
       console.log('\x1b[36m%s\x1b[0m', message) // cyan
       return null
     },
-    log_step(message: string) {
+    cypress_witch_log_step(message: string) {
       console.log('\x1b[34m%s\x1b[0m', message) // blue
       return null
     },
-    log_expected_result(message: string) {
+    cypress_witch_log_expected_result(message: string) {
       console.log('\x1b[35m%s\x1b[0m', message) // magenta
       return null
     },
